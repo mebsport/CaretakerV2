5711f98ca64ced8d571b115b35949509cb52cf0e
Marcelus <mebsport@gmail.com>
2020-04-12 14:20:56 -0400

    intergrated music and upgraded main menu
    intergrated movement on main menu, added music to the two menu's, a started working on a points system

---
+++ Assets/Scripts/MenuMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMovement : MonoBehaviour
+{
+    // Variables
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    Vector2 targetPosition;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = GetRandomPosition();
+    }
+
+
+    void Update()
+    {
+        // Move to random place
+        if ((Vector2)transform.position != targetPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed);
+        }
+        else
+        {
+            targetPosition = GetRandomPosition();
+        }
+    }
+    Vector2 GetRandomPosition()
+    {
+        // Get random position on screen
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxX);
+        return new Vector2(randomX, randomY);
+    }
+}
