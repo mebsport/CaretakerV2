a4c1ab22cac94b6e826e3493f80eae7cc9145fd0
Marcelus <mebsport@gmail.com>
2020-04-13 13:05:39 -0400

    Re enabled test mode
    re enabled test mode so we wouldnt run into any problems with accidnentily playing an ad while in unity

--- Assets/Scripts/AdManager.cs
+++ Assets/Scripts/AdManager.cs
@@ -7,7 +7,7 @@ public class AdManager : MonoBehaviour
 {
 
     string GooglePlay_ID = "3330924";
-    bool TestMode = false;
+    bool TestMode = true;
 
     // Start is called before the first frame update
     void Start()
